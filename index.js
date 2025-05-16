cc123f1f242b259b4b31ef16c0255cd3d84666ca
prabhdeep.multani <prabhdeep.multani@exelab.com>
2025-05-16 14:45:34 +0200

    Implement homepage and updates routes with LOTR character data handling
    - Added a new homepage route to fetch and display LOTR character data from HubSpot API.
    - Created a form for adding new characters with a corresponding route to handle submissions.
    - Updated environment variable usage for private app access token.
    - Introduced new Pug templates for the homepage and character updates, enhancing the UI with Tailwind CSS and Font Awesome.

--- index.js
+++ index.js
@@ -1,71 +1,81 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const app = express();
+require("dotenv").config();
 
-app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "pug");
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS_TOKEN;
+
+const HS_API_BASE_URL = "https://api.hubspot.com";
+const HS_OBJECT_TYPE = "2-142796098";
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get("/", async (req, res) => {
+  const lotrCharacters = `${HS_API_BASE_URL}/crm/v3/objects/${HS_OBJECT_TYPE}?properties=name,race,biography`;
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    "Content-Type": "application/json",
+  };
+  try {
+    const resp = await axios.get(lotrCharacters, { headers });
+    const data = resp.data.results;
+    res.render("homepage", {
+      title: "LOTR Characters Homepage | Integrating With HubSpot I Practicum",
+      data,
+      error: req.query.error,
+    });
+  } catch (error) {
+    console.error("Error fetching LOTR characters:", error);
+    res.render("homepage", {
+      title: "LOTR Characters Homepage | Integrating With HubSpot I Practicum",
+      data: [],
+      error: "Failed to load characters",
+    });
+  }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get("/update-cobj", (req, res) => {
+  res.render("updates", {
+    title: "Update Custom Object Form | Integrating With HubSpot I Practicum",
+  });
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+app.post("/update-cobj", async (req, res) => {
+  const createUrl = `${HS_API_BASE_URL}/crm/v3/objects/${HS_OBJECT_TYPE}`;
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    "Content-Type": "application/json",
+  };
 
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
+  const characterData = {
+    properties: {
+      name: req.body.name,
+      race: req.body.race,
+      biography: req.body.biography,
+    },
+  };
 
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    await axios.post(createUrl, characterData, { headers });
+    res.redirect("/");
+  } catch (error) {
+    console.error("Create Error:", error.response?.data || error.message);
+    res.render("updates", {
+      title: "Create LOTR Character",
+      error: "Failed to create character",
+    });
+  }
 });
-
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
-});
-*/
-
 
 // * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.listen(3000, () => console.log("Listening on http://localhost:3000"));
